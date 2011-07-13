86d19937bb6c39d8254cf9e007f8bb7506de5c0d
Alex Fortuna <alex.r@askit.org>
2011-07-13 16:13:07 +0400

    Rails 2 generator fix.

--- generators/rails_dt/rails_dt_generator.rb
+++ generators/rails_dt/rails_dt_generator.rb
@@ -1,8 +1,8 @@
 class RailsDtGenerator < Rails::Generator::Base   #:nodoc:
   def manifest
     record do |m|
-      m.file (fn = "dt.css"), "public/stylesheets/#{fn}"
-      m.file (fn = "dt.rb"), "config/initializers/#{fn}"
+      m.file((fn = "dt.css"), "public/stylesheets/#{fn}")
+      m.file((fn = "dt.rb"), "config/initializers/#{fn}")
       m.readme "INSTALL"
     end
   end
