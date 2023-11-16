566ba14fad19f4da5ba84896613334bc52504bb1
Alex Fortuna <fortunadze@gmail.com>
2023-11-17 01:24:09 +0300

    Moving on

--- spec/lib/dy/_context.rb
+++ spec/lib/dy/_context.rb
@@ -10,6 +10,8 @@ shared_context __dir__ do
       allow(conf).to receive(:console).and_return(conf_console)
       allow(conf).to receive(:log).and_return(conf_log)
       allow(conf).to receive(:rails).and_return(conf_rails)
+
+      # OPTIMIZE: Import `stub_of` from WC.
       allow(conf_console).to receive(:enabled).and_return(of_conf_console_enabled) if defined?(of_conf_console_enabled)
       allow(conf_log).to receive(:enabled).and_return(of_conf_log_enabled) if defined?(of_conf_log_enabled)
       allow(conf_rails).to receive(:enabled).and_return(of_conf_rails_enabled) if defined?(of_conf_rails_enabled)
