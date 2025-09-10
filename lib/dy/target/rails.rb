bdba005048d374f96e8da303f9edab395235bc7b
Alex Fortuna <fortunadze@gmail.com>
2025-09-10 11:30:51 +0300

    Moving on, late change

--- lib/dy/target/rails.rb
+++ lib/dy/target/rails.rb
@@ -11,8 +11,16 @@ module DY; module Target
     # Print a full message.
     # @param [String] fullmsg
     def print(fullmsg)
-      require_attr :rails
-      rails.logger.debug(fullmsg)
+      # Rails is procedural and CAN be in a state where `Rails.logger` is nil.
+      # Check it upon every invocation to prevent unwanted crashes.
+      # For the same reason we'd rather not memoize `rails.logger` into a lazy attribute.
+      logger = require_attr(:rails).logger
+      if logger
+        logger.debug(fullmsg)
+      else
+        # OPTIMIZE: Provide test coverage.
+        "hop, hey hop"
+      end
     end
   end
 end; end
